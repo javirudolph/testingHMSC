01a27cf6585d343a97471785167646d03eaed0e2
javirudolph <javiera.av@ufl.edu>
2019-02-20 09:28:37 -0500

    Editing the RDS files and params

--- update1_jan18/dataWrangling/wranglingFunctions.R
+++
@@ -1,5 +0,0 @@
-# This script provides the functions necessary to edit, organize, clean and format the output data from VariPart in the HMSC package
-
-# The structure of the files will determine which function to use, as species level or patch level have a different number of hierarchies in the data
-
-# The parameters associated to each of these scenarios has to be brough in from the simulation component, which requires use of initial scripts. 
