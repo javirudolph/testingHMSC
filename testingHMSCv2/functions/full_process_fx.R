1f5d2aa45d83722c6d788ea047b165b3967062b2
javirudolph <javiera.av@ufl.edu>
2019-02-26 11:50:04 -0500

    Editing functions and testing them.

--- testingHMSCv2/functions/full_process_fx.R
+++ testingHMSCv2/functions/full_process_fx.R
@@ -15,7 +15,8 @@
 metacom_sim4HMSC <- function(XY, E, pars, nsteps,
                              occupancy, niter,
                              makeRDS = FALSE,
-                             whereToSave = NULL){
+                             whereToSave = NULL,
+                             objName = NULL){
   
   N <- pars$N
   D <- pars$D
@@ -30,13 +31,115 @@ metacom_sim4HMSC <- function(XY, E, pars, nsteps,
   }
   
   if(makeRDS == TRUE){
-    saveRDS(res, file = whereToSave)
+    nameFile <- paste0(whereToSave, objName, ".RDS")
+    saveRDS(res, file = nameFile)
   }
   
   return(res)
 
 }
 
+
+
+# Fit HMSC ----------------------------------------------------------------
+
+metacom_as_HMSCdata <- function(metacomData, numClusters, E, MEMsel,
+                                makeRDS = FALSE,
+                                whereToSave = NULL,
+                                objName = NULL){
+  
+  N <- nrow(E)
+  
+  run <- metacomData
+  nrun <- length(run)
+  
+  clusters <- makeCluster(numClusters)
+  registerDoParallel(clusters)
+  
+  ### Estimate models
+  model <- foreach(j = 1:nrun) %dopar% {
+    library(HMSC)
+    formData <- as.HMSCdata(Y = run[[j]], X = cbind(scale(E),scale(E)^2, MEMsel),
+                            Random = as.factor(1:N),
+                            scaleX = TRUE, interceptX = TRUE)
+    
+    hmsc(formData, family = "probit",
+         niter = 10000, nburn = 5000, thin = 5)
+  }
+  
+  ### Stop clusters
+  stopCluster(clusters)
+  
+  if(makeRDS == TRUE){
+    nameFile <- paste0(whereToSave, objName, ".RDS")
+    saveRDS(model, file = nameFile)
+  }
+  
+  return(model)
+}
+
+
+
+# Variation Partitioning --------------------------------------------------
+
+get_VPresults <- function(HMSCmodel, MEMsel,
+                   makeRDS = FALSE,
+                   whereToSave = NULL,
+                   objName = NULL){
+  
+  model <- HMSCmodel
+  nmodel <- length(model)
+  
+  clusters <- makeCluster(ncluster)
+  registerDoParallel(clusters)
+  
+  ### Estimate models
+  vpRes <- foreach(j = 1:nmodel) %dopar% {
+    library(HMSC)
+    variPart(model[[j]], groupX = c(rep("env",3),rep("spa",length(MEMsel))), 
+             type = "III", R2adjust = TRUE)
+  }
+  
+  ### Stop clusters
+  stopCluster(clusters)
+  
+  if(makeRDS == TRUE){
+    nameFile <- paste0(whereToSave, objName, ".RDS")
+    saveRDS(vpRes, file = nameFile)
+  }
+  
+  return(vpRes)
+}
+
+get_VPresults_SITE <- function(HMSCmodel, MEMsel,
+                               makeRDS = FALSE,
+                               whereToSave = NULL,
+                               objName = NULL){
+  
+  model <- HMSCmodel
+  nmodel <- length(model)
+  
+  clusters <- makeCluster(ncluster)
+  registerDoParallel(clusters)
+  
+  ### Estimate models
+  vpRes <- foreach(j = 1:nmodel) %dopar% {
+    library(HMSC)
+    variPart(model[[j]], groupX = c(rep("env",3),rep("spa",length(MEMsel))),
+             indSite = TRUE,
+             type = "III", R2adjust = TRUE)
+  }
+  
+  ### Stop clusters
+  stopCluster(clusters)
+  
+  if(makeRDS == TRUE){
+    nameFile <- paste0(whereToSave, objName, ".RDS")
+    saveRDS(vpRes, file = nameFile)
+  }
+  
+  return(vpRes)
+}
 
 
 
--- testingHMSCv2/runScripts/testingProcessRun.R
+++ testingHMSCv2/runScripts/testingProcessRun.R
@@ -1,6 +1,13 @@
 
 # Testing the whole process from the functions developed in the 'full_process_fx.R'
 
+# Libraries used:
+
+library(HMSC)
+library(doParallel)
+library(adespatial)
+library(tidyverse)
+library(ggtern)
 
 
 # Source functions --------------------------------------------------------
@@ -15,8 +22,89 @@ source("functions/metacom_sim_fx.R")
 source("functions/main_sim_fx.R")
 
 # You could generate a landscape with the functions provided in:
-# source("functions/main_sim_fx.R")
-# Or use the fixed landscape and MEMs from
+source("functions/landscape_fx.R")
+# We are doing this, since this is just a test run, with a smaller number of sites and species richness
+
+# Or use the fixed landscape and MEMs from - beware this one takes a while
 # source("runScripts/fixedLandscape_and_MEMs.R")
 # Or, if available locally, just load the RDS files saved from that script in the outputs folder
 # These RDS files should include the XY, E and MEMsel
+
+# This are the functions that run the whole process
+source("functions/full_process_fx.R")
+
+
+
+# Test cycle of all functions ---------------------------------------------
+
+###########################################################################
+# 1. Environment
+N <- 300
+D <- 1
+R <- 7
+
+testXY <- get_xy(N)
+testE <- get_E(D, N)
+
+# MEMs section is for later in the fitting, but should probably just do it now... 
+
+ncluster <- 4
+testMEM <- dbmem(testXY, MEM.autocor = "positive", thresh = 0.5)
+
+if(FALSE){
+  ### Set clusters
+  clusters <- makeCluster(ncluster)
+  registerDoParallel(clusters)
+  
+  ### test MEM autocorrelation
+  testedMEM <- foreach(i=1:ncol(testMEM)) %dopar% {
+    adespatial:::moran.randtest(testMEM[,i], attributes(testMEM)$listw, nrepet = 9999)
+  }
+  
+  ### Stop clusters
+  stopCluster(clusters)
+}
+
+pvals <- lapply(testedMEM, function(x) x$pvalue)
+# min(which(pvals>0.05))
+
+lastSignificant <- (min(which(pvals>0.05)) - 1)
+
+# Select the MEMs based on their significance and then save that result
+testMEMsel <- testMEM[, 1:lastSignificant]
+
+
+############################################################################
+# 2. We create the parameters
+
+testParams <- prep_pars(N = N, D = D, R = R, breadth = 0.2,
+                        alpha = 0.05, interx_col = 0, interx_ext = 0,
+                        makeRDS = TRUE,
+                        whereToSave = "outputs/testOutputs/", objName = "testPars")
+
+
+###########################################################################
+# 3. Metacommunity simulation with iterations
+testMetacom <- metacom_sim4HMSC(XY = testXY, E = testE, pars = testParams,
+                                nsteps = 20, occupancy = 0.8, niter = 3,
+                                makeRDS = TRUE, whereToSave = "outputs/testOutputs/", objName = "testMetacom")
+
+
+##########################################################################
+# 4. Fit/Format HSMC to our simulated data. Remember you need the MEMsel for this
+
+testHMSCmodel <- metacom_as_HMSCdata(metacomData = testMetacom, numClusters = 4, E = testE,
+                                     MEMsel = testMEMsel, makeRDS = TRUE,
+                                     whereToSave = "outputs/testOutputs/",
+                                     objName = "testHMSCmodel")
+
+##########################################################################
+# 5. Get variation partitioning components
+testVPresults <- get_VPresults(HMSCmodel = testHMSCmodel, MEMsel = testMEMsel,
+                               makeRDS = TRUE,
+                               whereToSave = "outputs/testOutputs/",
+                               objName = "testVPresults")
+
+
+
+
