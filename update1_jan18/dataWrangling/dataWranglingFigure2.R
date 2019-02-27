fb757aea243dd1cad30f6b2f248627963111b1d8
javirudolph <javiera.av@ufl.edu>
2019-02-27 10:42:53 -0500

    Edit to include prevalence by site

--- update1_jan18/dataWrangling/dataWranglingFigure2.R
+++ update1_jan18/dataWrangling/dataWranglingFigure2.R
@@ -66,10 +66,23 @@ variPartTidy <- function(VPdata, params){
   }
   
   fullData %>% 
-    bind_rows() -> fullData
+    bind_rows() %>% 
+    mutate(identifier = paste0("spp", species, "_", iteration)) -> fullData
   return(fullData)
 }
 
+# Get the prevalence
+prevalenceForSpp <- function(filenameWithPath){
+
+  readRDS(paste(filenameWithPath)) %>% 
+    set_names(imap(., ~ paste0("iter_", .y))) %>% 
+    map(., colSums) %>%
+    bind_cols() %>% 
+    rownames_to_column(var = "species") %>% 
+    gather(., key = "iteration", value = "prevalence", -species) %>% 
+    mutate(identifier = paste0("spp", species, "_", iteration)) %>% 
+    select(., -c(species, iteration))
+}
 
 # Fig2a -------------------------------------------------------------------
 
@@ -77,15 +90,18 @@ datFig2a <- dataLoader("Fig2a")
 prmsFilenam <- "update1_jan18/scenario1Fig2a.R"
 prmsFig2a <- compileFig2Params(prmsFilenam)
 fullFig2a <- variPartTidy(datFig2a, prmsFig2a) %>% 
-  mutate(scenario = "Fig2a")
-
+  mutate(scenario = "Fig2a") %>% 
+  left_join(., prevalenceForSpp("update1_jan18/Fig2a_run.RDS"), by = "identifier")
 
 # Fig2b -------------------------------------------------------------------
 datFig2b <- dataLoader("Fig2b")
 prmsFilenam <- "update1_jan18/scenario2Fig2b.R"
 prmsFig2b <- compileFig2Params(prmsFilenam)
 fullFig2b <- variPartTidy(datFig2b, prmsFig2b) %>% 
-  mutate(scenario = "Fig2b")
+  mutate(scenario = "Fig2b") %>% 
+  left_join(., prevalenceForSpp("update1_jan18/Fig2b_run.RDS"), by = "identifier")
+
+
 
 
 # Fig2c -------------------------------------------------------------------
@@ -94,7 +110,8 @@ datFig2c <- dataLoader("Fig2c")
 prmsFilenam <- "update1_jan18/scenario3Fig2c.R"
 prmsFig2c <- compileFig2Params(prmsFilenam)
 fullFig2c <- variPartTidy(datFig2c, prmsFig2c) %>% 
-  mutate(scenario = "Fig2c")
+  mutate(scenario = "Fig2c") %>% 
+  left_join(., prevalenceForSpp("update1_jan18/Fig2c_run.RDS"), by = "identifier")
 
 
 # Fig2d -------------------------------------------------------------------
@@ -103,11 +120,16 @@ datFig2d <- dataLoader("Fig2d")
 prmsFilenam <- "update1_jan18/scenario4Fig2d.R"
 prmsFig2d <- compileFig2Params(prmsFilenam)
 fullFig2d <- variPartTidy(datFig2d, prmsFig2d) %>% 
-  mutate(scenario = "Fig2d")
+  mutate(scenario = "Fig2d") %>% 
+  left_join(., prevalenceForSpp("update1_jan18/Fig2d_run.RDS"), by = "identifier")
 
 
 # Compile all -------------------------------------------------------------
 
 Figure2Data <- bind_rows(fullFig2a, fullFig2b, fullFig2c, fullFig2d)
-#write.csv(Figure2Data, file = "update1_jan18/dataWrangling/Fig2DataParams.csv")
-#saveRDS(Figure2Data, file = "update1_jan18/newFigures/Fig2DataParams.RDS")
+write.csv(Figure2Data, file = "update1_jan18/dataWrangling/Fig2DataParams.csv")
+saveRDS(Figure2Data, file = "update1_jan18/newFigures/Fig2DataParams.RDS")
+
+
+
+         
