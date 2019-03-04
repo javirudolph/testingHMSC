0af98176e43253a704059828be30b1f4cfc7ce38
javirudolph <javiera.av@ufl.edu>
2019-03-04 12:51:23 -0500

    Got two functions in one, incorporates if statement to change between species or site level.

--- testingHMSCv2/functions/data_wrangling_fx.R
+++ testingHMSCv2/functions/data_wrangling_fx.R
@@ -2,8 +2,70 @@
 
 # Functions to modify and organize the dataframes from the VP output and get ready for plotting
 
-
-doItAll_twosppNichecomp <- function(outPath, scenarioNum){
+doItAll_twosppNichecomp <- function(outPath, scenarioNum, indSites = FALSE){
+  
+  
+  if(indSites == TRUE){
+    richness <- readRDS(paste0(outPath, scenarioNum, "-metacomSim.RDS")) %>% 
+      set_names(imap(., ~ paste0("iter", .y))) %>% 
+      map(., rowSums) %>%
+      bind_rows() %>% 
+      rownames_to_column(var = "sites") %>% 
+      gather(., key = "iteration", value = "richness", -sites) %>% 
+      mutate(identifier = paste0("site", sites, "_", iteration)) %>% 
+      select(., -c(sites, iteration))
+    
+    
+    
+    vp <- readRDS(paste0(outPath, scenarioNum, "-vpsites.RDS"))
+    
+    overlap1 <- map(vp, "overlap1")
+    overlap2 <- map(vp, "overlap2")
+    overlap3 <- map(vp, "overlap3")
+    
+    vpALL <- vector("list", length = 5)
+    for(i in 1:5){
+      workingVP1 <- overlap1[[i]]
+      workingVP2 <- overlap2[[i]]
+      workingVP3 <- overlap3[[i]]
+      
+      c <- rowSums(workingVP1[,,1])/15
+      b <- rowSums(workingVP1[,,2])/15
+      a <- rowSums(workingVP1[,,3])/15
+      
+      e <- rowSums(workingVP2[,,1])/15
+      f <- rowSums(workingVP2[,,2])/15
+      d <- rowSums(workingVP2[,,3])/15
+      
+      g <- rowSums(workingVP3)/15
+      
+      env <- a + f + 1/2 * d + 1/2 * g
+      env <- ifelse(env < 0, 0, env)
+      spa <- b + e + 1/2 * d + 1/2 * g
+      spa <- ifelse(spa < 0, 0, spa)
+      random <- c
+      codist <- ifelse(random < 0, 0, random)
+      r2 <- env + spa + codist
+      iteration <- factor(paste0("iter", i), levels = paste0("iter", 1:5))
+      
+      cleanData <- cbind.data.frame(env, spa, codist, r2, iteration)
+      cleanData$site <- paste0(row.names(cleanData))
+      
+      vpALL[[i]] <- cleanData
+    }
+    
+    vpALL %>% 
+      bind_rows() %>% 
+      mutate(identifier = paste(site, iteration, sep = "_")) %>% 
+      left_join(., richness) -> vpALL
+    
+    return(vpALL)
+  }
+  
+  
+  
+  
+  
   params <- with(readRDS(paste0(outPath, scenarioNum, "-params.RDS")), {
     enframe(u_c[1,], name = "species",value =  "nicheOpt") %>% 
       left_join(., enframe(s_c[1,], name = "species", value =  "nicheBreadth")) %>% 
@@ -53,74 +115,7 @@ doItAll_twosppNichecomp <- function(outPath, scenarioNum){
     bind_rows() %>% 
     mutate(identifier = paste0("spp", species, "_", iteration), 
            scenario = paste0("scenario", scenarioNum)) %>% 
-    left_join(., prevalence)
-  
-}
-
-
-
-
-
-
-# BY SITES ----------------------------------------------------------------
-
-
-
-bysitesDoItAll <- function(outPath, scenarioNum){
-  
-  
-  richness <- readRDS(paste0(outPath, scenarioNum, "-metacomSim.RDS")) %>% 
-    set_names(imap(., ~ paste0("iter", .y))) %>% 
-    map(., rowSums) %>%
-    bind_rows() %>% 
-    rownames_to_column(var = "sites") %>% 
-    gather(., key = "iteration", value = "richness", -sites) %>% 
-    mutate(identifier = paste0("site", sites, "_", iteration)) %>% 
-    select(., -c(sites, iteration))
-  
-  
-  
-  vp <- readRDS(paste0(outPath, scenarioNum, "-vpsites.RDS"))
-  
-  overlap1 <- map(vp, "overlap1")
-  overlap2 <- map(vp, "overlap2")
-  overlap3 <- map(vp, "overlap3")
-  
-  vpALL <- vector("list", length = 5)
-  for(i in 1:5){
-    workingVP1 <- overlap1[[i]]
-    workingVP2 <- overlap2[[i]]
-    workingVP3 <- overlap3[[i]]
-    
-    c <- rowSums(workingVP1[,,1])/15
-    b <- rowSums(workingVP1[,,2])/15
-    a <- rowSums(workingVP1[,,3])/15
-    
-    e <- rowSums(workingVP2[,,1])/15
-    f <- rowSums(workingVP2[,,2])/15
-    d <- rowSums(workingVP2[,,3])/15
-    
-    g <- rowSums(workingVP3)/15
-    
-    env <- a + f + 1/2 * d + 1/2 * g
-    env <- ifelse(env < 0, 0, env)
-    spa <- b + e + 1/2 * d + 1/2 * g
-    spa <- ifelse(spa < 0, 0, spa)
-    random <- c
-    codist <- ifelse(random < 0, 0, random)
-    r2 <- env + spa + codist
-    iteration <- factor(paste0("iter", i), levels = paste0("iter", 1:5))
-    
-    cleanData <- cbind.data.frame(env, spa, codist, r2, iteration)
-    cleanData$site <- paste0(row.names(cleanData))
-    
-    vpALL[[i]] <- cleanData
-  }
-  
-  vpALL %>% 
-    bind_rows() %>% 
-    mutate(identifier = paste(site, iteration, sep = "_")) %>% 
-    left_join(., richness)
-
+    left_join(., prevalence) -> fullData
+  return(fullData)
   
 }
