bffce894786032165de0b15d31103013cbb44d73
javirudolph <javiera.av@ufl.edu>
2019-03-09 19:51:50 -0500

    Running the two species comparisons with a quadratic term

---
+++ testingHMSCv2/runScripts/two_spp_niche_comparisons_quadratic.R
@@ -0,0 +1,181 @@
+# This script is used to run several models with two species, and changing their niche optima
+
+
+# Libraries ---------------------------------------------------------------
+# Load the libraries
+library(HMSC)
+library(doParallel)
+library(tidyverse)
+library(ggtern)
+
+
+# Functions ---------------------------------------------------------------
+# Load the functions
+source("functions/prep_pars_fx.R")
+source("functions/metacom_sim_fx.R")
+source("functions/main_sim_fx.R")
+source("functions/full_process_fx.R")
+source("functions/data_wrangling_fx.R")
+source("functions/make_tern_plot_fx.R")
+
+
+# Replace with quadratic --------------------------------------------------
+# I'm basically overwriting the original function with this one that has the quadratic term. 
+# It'snot an elegant way, but we can make an if statement later on, so you can choose which one to go with. 
+S_f <- function(E, u_c, s_c) {
+  R <- ncol(u_c)
+  N <- nrow(E)
+  D <- ncol(E)
+  S <- matrix(1, nr = N, nc = R)
+  for(i in 1:D){
+    optima <- matrix(u_c[i,],nrow = N,ncol = R,byrow = TRUE)
+    breadth <- matrix(s_c[i,],nrow = N,ncol = R,byrow = TRUE)
+    S <- S * ((-1 / (breadth/2)^2) * (E[,i] - optima)^2 + 1)
+  }
+  return(S)
+}
+
+
+# OrigLandscape -----------------------------------------------------------
+
+XY <- readRDS("outputs/fixedLandscapes/orig-no-seed-XY.RDS")
+E <- readRDS("outputs/fixedLandscapes/orig-no-seed-E.RDS")
+MEMsel <- readRDS("outputs/fixedLandscapes/orig-no-seed-MEMsel.RDS")
+
+
+
+# RunCycles ---------------------------------------------------------------
+# These steps should happen for each combination of the niche optima parameters
+# 1. Prepare Parameters
+# 2. Simulate Metacommunity
+# 3. Fit/Format HMSC
+# 4. VP for species
+# 5. VP for sites
+# 6. Organize dataframes for plotting
+# 7. Plot
+
+
+
+# This loop would repeat for each niche scenario...
+# Problem with identifiers - using the niche optima has a decimal point, filenames don't like periods.
+# Label them in progression? both 0.5 is scenario1?
+
+
+# Create Folder to save ---------------------------------------------------
+
+
+savedate <- format(Sys.Date(), "%Y%m%d")
+folderpath <- paste0("outputs/", savedate, "-two_spp_niche_comparisons/")
+
+if(dir.exists(folderpath) == FALSE){
+  dir.create(folderpath)
+}
+
+# Species Niche Optima ----------------------------------------------------
+
+spp1niche <- seq(from = 0.5, to = 0.9, by = 0.05)
+spp2niche <- rev(seq(from = 0.1, to = 0.5, by = 0.05))
+
+
+# u_c <- matrix(c(spp1niche[2], spp2niche[2]), nrow = 1, ncol = 2)
+
+#########################################################################################################
+# This chunk runs the cycle and it takes a long time to finish. Run at night.
+
+for(j in 1:length(spp1niche)){
+  nicheOpt2spp <- matrix(c(spp1niche[j], spp2niche[j]), ncol = 2, nrow = 1)
+  namesrds <- paste0("scenario", j)
+
+  pars <- prep_pars(N = 1000, D = 1, R = 2,
+                    nicheOpt = nicheOpt2spp, breadth = 0.2, alpha = 0.005,
+                    interx_col = 0, interx_ext = 0, makeRDS = TRUE, whereToSave = folderpath, objName = namesrds)
+
+  sims <- metacom_sim4HMSC(XY = XY, E = E, pars = pars,
+                            nsteps = 200, occupancy = 0.8, niter = 5,
+                           makeRDS = TRUE, whereToSave = folderpath, objName = namesrds)
+
+  model <- metacom_as_HMSCdata(sims, numClusters = 4, E = E, MEMsel = MEMsel,
+                               makeRDS = TRUE, whereToSave = folderpath, objName = namesrds)
+
+  vpSpp <- get_VPresults(model, MEMsel = MEMsel, numClusters = 4,
+                         makeRDS = TRUE, whereToSave = folderpath, objName = namesrds)
+
+  vpSites <- get_VPresults_SITE(model, MEMsel = MEMsel, numClusters = 4,
+                           makeRDS = TRUE, whereToSave = folderpath, objName = namesrds)
+
+}
+
+
+
+# Organize and Plot -------------------------------------------------------
+make_tern_plot <- function(figData, varShape = NULL, varColor = NULL){
+  figData %>% 
+    ggtern(aes(x = env, z = spa, y = codist, size = r2)) +
+    scale_T_continuous(limits=c(0,1.0),
+                       breaks=seq(0,1,by=0.1),
+                       labels=seq(0,1,by=0.1)) +
+    scale_L_continuous(limits=c(0.0,1),
+                       breaks=seq(0,1,by=0.1),
+                       labels=seq(0,1,by=0.1)) +
+    scale_R_continuous(limits=c(0.0,1.0),
+                       breaks=seq(0,1,by=0.1),
+                       labels=seq(0,1,by=0.1)) +
+    labs(x = "Environment",
+         y = "Codistribution",
+         z = "Spatial") +
+    geom_point(aes_string(shape = varShape, color = varColor), alpha = 0.7) +
+    #scale_color_viridis_c() +
+    theme_minimal() +
+    guides(size = guide_legend(order = 1,
+                               title = expression(paste(R^{2}))),
+           shape = guide_legend("none"),
+           # shape = guide_legend(order = 2,
+           #                      title = varShape,
+           #                      override.aes = list(size = 3)),
+           col = guide_colourbar(title = varColor,
+                                 order = 3)) +
+    theme(#legend.position = "bottom", legend.box = "vertical",
+      panel.grid = element_line(color = "darkgrey"),
+      axis.title = element_text(size = 7))
+}
+
+save_csv_and_plots <- function(scenario){
+  sppcsv <- doItAll_twosppNichecomp(outPath = folderpath, scenarioNum = scenario, indSites = FALSE)
+  #write.csv(sppcsv, file = paste0(folderpath, "csvFiles/", scenario, "spp.csv"))
+  sppcsv %>% 
+    make_tern_plot(., varShape = "iteration", varColor = "nicheOpt") +
+    labs(title = scenario)
+  ggsave(filename = paste0(folderpath, "figures/", scenario, "spp.png"), dpi = 300, width = 9, height = 4.5)
+  
+  sitescsv <- doItAll_twosppNichecomp(outPath = folderpath, scenarioNum = scenario, indSites = TRUE)
+  write.csv(sitescsv, file = paste0(folderpath, "csvFiles/", scenario, "sites.csv"))
+  make_tern_plot(sitescsv, varShape = "iteration", varColor = "richness") +
+    labs(title=scenario)
+  ggsave(filename = paste0(folderpath, "figures/", scenario, "sites.png"), dpi = 300, width = 9, height = 4.5)
+  
+}
+
+for(i in 1:9){
+  loopscen <- paste0("scenario", i)
+  save_csv_and_plots(loopscen)
+  print(i)
+}
+
+
+
+# All in one plot ---------------------------------------------------------
+
+allData <- NULL
+for(i in 1:9){
+  scenario <- paste0("scenario", i)
+  onescenario <- doItAll_twosppNichecomp(outPath = folderpath, scenario)
+  allData <- bind_rows(allData, onescenario)
+  
+}
+
+allData %>% 
+  make_tern_plot(., varColor = "nicheOpt") +
+  scale_color_viridis_c()
+
+
+
