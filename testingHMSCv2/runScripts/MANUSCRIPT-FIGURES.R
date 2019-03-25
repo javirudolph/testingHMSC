12245db968ec42b397fcf2ee00b487ead5768799
javirudolph <javiera.av@ufl.edu>
2019-03-25 10:38:00 -0400

    Editing functions, etc to get Figures for manuscript

---
+++ testingHMSCv2/runScripts/MANUSCRIPT-FIGURES.R
@@ -0,0 +1,248 @@
+
+# INTRO -------------------------------------------------------------------
+# FIGURES 2 AND 3 - IMAGE FILES
+# Created by: Javiera Rudolph
+# Date: March 25, 2019
+
+# Warning: this is going to be a horribly long script with a bunch of functions at the beginning.
+# I figured it would just be better to keep all the stuff in here, instead of trying to source a 
+# script with functions specific to this manuscript's figures.
+# You can use the outline feature in RStudio, to jump between the different sections.
+
+library(tidyverse)
+library(ggtern)
+
+
+
+# FUNCTIONS ---------------------------------------------------------------
+
+
+# ** get_sites_data() to organize site level data and get ready for plot
+
+    # This function takes the simulated metacommunity to get the species richness for each site
+    # It then binds that information to the Variation Partinioning output from the HMSC::variPart()
+    # This function also takes the output of HMSC::variPart() and organizes each fraction:
+    #   into the env, spa and codist components
+    # The output of the get_sites_data() function is a dataframe that is ready to plot
+
+get_sites_data <- function(folderpath, scenario){
+  richness <- readRDS(paste0(folderpath, scenario, "-metacomSim.RDS")) %>% 
+    set_names(imap(., ~ paste0("iter", .y))) %>% 
+    map(., rowSums) %>%
+    bind_rows() %>% 
+    rownames_to_column(var = "sites") %>% 
+    gather(., key = "iteration", value = "richness", -sites) %>% 
+    mutate(identifier = paste0("site", sites, "_", iteration)) %>% 
+    dplyr::select(., -c(sites, iteration))
+  
+
+  vp <- readRDS(paste0(folderpath, scenario, "-vpsites.RDS"))
+  
+  overlap1 <- map(vp, "overlap1")
+  overlap2 <- map(vp, "overlap2")
+  overlap3 <- map(vp, "overlap3")
+  
+  vpALL <- vector("list", length = 5)
+  for(i in 1:5){
+    workingVP1 <- overlap1[[i]]
+    workingVP2 <- overlap2[[i]]
+    workingVP3 <- overlap3[[i]]
+    
+    c <- rowSums(workingVP1[,,1])/15
+    b <- rowSums(workingVP1[,,2])/15
+    a <- rowSums(workingVP1[,,3])/15
+    
+    e <- rowSums(workingVP2[,,1])/15
+    f <- rowSums(workingVP2[,,2])/15
+    d <- rowSums(workingVP2[,,3])/15
+    
+    g <- rowSums(workingVP3)/15
+    
+    env <- a + f + 1/2 * d + 1/2 * g
+    env <- ifelse(env < 0, 0, env)
+    spa <- b + e + 1/2 * d + 1/2 * g
+    spa <- ifelse(spa < 0, 0, spa)
+    random <- c
+    codist <- ifelse(random < 0, 0, random)
+    r2 <- env + spa + codist
+    iteration <- factor(paste0("iter", i), levels = paste0("iter", 1:5))
+    
+    cleanData <- cbind.data.frame(env, spa, codist, r2, iteration)
+    cleanData$site <- paste0(row.names(cleanData))
+    
+    vpALL[[i]] <- cleanData
+  }
+  
+  vpALL %>% 
+    bind_rows() %>% 
+    mutate(identifier = paste(site, iteration, sep = "_"),
+           scenario = scenario) %>% 
+    left_join(., richness)
+}
+
+
+# ** get_species_data() this function organizes species level data and gets it ready to plot.
+  # It takes the RDS file that is the output of the HMSC::variPart() function and reorganizes it into the 
+  # env, codist and spa components.
+  # It also calculates the prevalence information for each species. 
+
+get_species_data <- function(folderpath, scenario){
+  
+  prevalence <- readRDS(paste0(folderpath, scenario, "-metacomSim.RDS")) %>% 
+    set_names(imap(., ~ paste0("iter_", .y))) %>% 
+    map(., colSums) %>%
+    bind_cols() %>% 
+    rownames_to_column(var = "species") %>% 
+    gather(., key = "iteration", value = "prevalence", -species) %>% 
+    mutate(identifier = paste0("spp", species, "_", iteration),
+           prevalence = prevalence/1000) %>% 
+    dplyr::select(., -c(species, iteration))
+  
+  
+  
+  
+  readRDS(paste0(folderpath, scenario, "-vpspp.RDS")) %>% 
+    set_names(imap(., ~ paste0("iter_", .y))) -> VPdata
+  
+  fullData <- list()
+  for(i in 1:length(VPdata)){
+    fullData[[i]] <- VPdata[[i]] %>% 
+      map(as_tibble) %>%
+      bind_cols() %>% 
+      rownames_to_column() %>% 
+      set_names(c("species", "c", "b", "a", "e", "f", "d", "g")) %>% 
+      transmute(species = species,
+                env = a + f + 0.5 * d + 0.5 * g,
+                env = ifelse(env < 0, 0, env),
+                spa = b + e + 0.5 * d + 0.5 * g,
+                spa = ifelse(spa < 0, 0, spa),
+                codist = c,
+                codist = ifelse(codist < 0, 0, codist),
+                r2 = env + spa + codist,
+                iteration = names(VPdata[i]))
+    
+  }
+  
+  fullData %>% 
+    bind_rows() %>% 
+    mutate(identifier = paste0("spp", species, "_", iteration), 
+           scenario = scenario) %>% 
+    left_join(., prevalence)
+  
+}
+
+
+# ** get_fig2_params() This function is for Figure 2 only. 
+# It takes the RDS params file and organizes it from a list to a dataframe, so that it can be joined to the
+# Variation Partition results at the species level. That way we can know what niche values each species has.
+# The output of this function can be joined to the VP results at the species level and then plot
+
+get_fig2_params <- function(folderpath, scenario){
+  pars <- readRDS(paste0(folderpath, scenario, "-params.RDS"))
+  
+  with(pars, {enframe(u_c[1,], name = "species", value = "nicheOptima") %>% 
+      left_join(., enframe(s_c[1,], name = "species", value = "nicheBreadth")) %>% 
+      left_join(., enframe(c_0, name = "species", value = "colonizationProb")) %>% 
+      mutate(., dispersal = alpha,
+             species = as.character(species),
+             speciesChr = as.character(paste0("spp_", species)),
+             interCol = d_c ,
+             interExt = d_e)})
+}
+
+# ** get_fig3_params() this is for Figure 3 only. 
+# Figure 3 has three sets of parameters because we divided simulations into groups.
+# Half of the species in each simulation has interactions, and the other half doesn't.
+# Or, a third of the species is assigned a different dispersal level, etc...
+
+get_fig3_params <- function(folderpath, scenario){
+  parsList <- readRDS(paste0(folderpath, scenario, "-params.RDS"))
+  fullPars <-  data.frame()
+  for(i in 1:length(parsList)){
+    
+    nspp <- nrow(fullPars)
+    
+    i_pars <- with(parsList[[i]], {
+      enframe(u_c[1,], name = "species",value =  "nicheOpt") %>% 
+        left_join(., enframe(s_c[1,], name = "species", value =  "nicheBreadth")) %>% 
+        left_join(., enframe(c_0, name = "species", value =  "colProb")) %>% 
+        mutate(dispersal = alpha, 
+               species = as.character(species + nspp), 
+               intercol = d_c, 
+               interext = d_e)})
+    
+    
+    fullPars <- rbind(fullPars, i_pars)
+    
+  }
+  
+  return(fullPars)
+}
+
+
+
+
+
+
+# TESTING AREA ------------------------------------------------------------
+
+folderpath <- "outputs/MANUSCRIPT/"
+scenario <- "FIG2A"
+
+params <- get_fig2_params(folderpath, scenario)
+sppData <- get_species_data(folderpath, scenario)
+
+
+# PLOT --------------------------------------------------------------------
+
+sppData %>% 
+  ggtern(aes(x = env, z = spa, y = codist, size = r2, color = iteration)) +
+  geom_point() +
+  scale_T_continuous(limits=c(0,1.0),
+                     breaks=seq(0,1,by=0.1),
+                     labels=seq(0,1,by=0.1)) +
+  scale_L_continuous(limits=c(0.0,1),
+                     breaks=seq(0,1,by=0.1),
+                     labels=seq(0,1,by=0.1)) +
+  scale_R_continuous(limits=c(0.0,1.0),
+                     breaks=seq(0,1,by=0.1),
+                     labels=seq(0,1,by=0.1)) +
+  labs(x = "Environment",
+       y = "Codistribution",
+       z = "Spatial") +
+  scale_size_area(limits = c(0,1), breaks = seq(0,1,0.2)) +
+  guides(color = guide_legend("none"), 
+         size = guide_legend(title = expression(R^2))) +
+  theme(panel.grid = element_line(color = "darkgrey"),
+    axis.title = element_text(size = 7))
+  
+  
+
+ggtern(aes(x = env, z = spa, y = codist, size = r2)) +
+  scale_T_continuous(limits=c(0,1.0),
+                     breaks=seq(0,1,by=0.1),
+                     labels=seq(0,1,by=0.1)) +
+  scale_L_continuous(limits=c(0.0,1),
+                     breaks=seq(0,1,by=0.1),
+                     labels=seq(0,1,by=0.1)) +
+  scale_R_continuous(limits=c(0.0,1.0),
+                     breaks=seq(0,1,by=0.1),
+                     labels=seq(0,1,by=0.1)) +
+  labs(x = "Environment",
+       y = "Codistribution",
+       z = "Spatial") +
+  geom_point(aes_string(shape = varShape, color = varColor), alpha = 0.7) +
+  theme_minimal() +
+  guides(size = guide_legend(order = 1,
+                             title = expression(paste(R^{2}))),
+         shape = guide_legend("none"),
+         # shape = guide_legend(order = 2,
+         #                      title = varShape,
+         #                      override.aes = list(size = 3)),
+         col = guide_colourbar(title = varColor,
+                               order = 3)) +
+  theme(#legend.position = "bottom", legend.box = "vertical",
+    panel.grid = element_line(color = "darkgrey"),
+    axis.title = element_text(size = 7))
+
+
