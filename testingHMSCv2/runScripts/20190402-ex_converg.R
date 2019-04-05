ae1e69475b12cc62396b9512c2d5456368e4ebe9
javirudolph <javiera.av@ufl.edu>
2019-04-05 09:08:42 -0400

    Convergence tests on a previous simulated model

--- testingHMSCv2/runScripts/20190402-ex_converg.R
+++ testingHMSCv2/runScripts/20190402-ex_converg.R
@@ -56,7 +56,7 @@ model <- hmsc(formData, family = "probit",
 #saveRDS(model, file = "outputs/testOutputs/conv_modl.RDS")
 
 # Chage the structure of the fitted model to be able to use functions from 'coda'
-chain <- as.mcmc(model, parameters = "meansParamX")
+chain <- as.mcmc(model, parameters = "meansParamX", burning = TRUE)
 # Get the summary and trace/density plots for the estimated parameters
 summary(chain)
 plot(chain)
