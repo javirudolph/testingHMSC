287b2e924837fe15f79d2a02c52e43ddb1ae79c1
javirudolph <javiera.av@ufl.edu>
2019-04-22 13:11:00 -0400

    include the correlation plots

--- 2019_manuscript/functions/output_processing_fx.R
+++ 2019_manuscript/functions/output_processing_fx.R
@@ -233,3 +233,36 @@ sites_plot <- function(data, plotMain = NULL, colorVar = NULL, colorLegend = "no
     theme(panel.grid = element_line(color = "darkgrey"),
           axis.title = element_text(size = 8))
 }
+
+
+# To create the species correlation matrices:
+# This will mostly follow the code provided from the HMSC vignette, therefore it is not in a tidyverse framework and requires additional packages
+library(corrplot)
+
+interactionPlot <- function(folderpath, scenario, iteration = NULL){
+  
+  if(is.null(iteration) == TRUE){
+    iteration <- 1
+  }
+  
+  
+  modelfile <- readRDS(paste0(folderpath, scenario, "-model.RDS"))
+  assoMat <- corRandomEff(modelfile[[iteration]])
+  siteMean <- apply(assoMat[ , , , 1], 1:2, mean)
+  
+  siteDrawCol <- matrix(NA, nrow = nrow(siteMean), ncol = ncol(siteMean))
+  siteDrawCol[which(siteMean > 0.4, arr.ind=TRUE)]<-"red"
+  siteDrawCol[which(siteMean < -0.4, arr.ind=TRUE)]<-"blue"
+  
+  # Build matrix of "significance" for corrplot
+  siteDraw <- siteDrawCol
+  siteDraw[which(!is.na(siteDraw), arr.ind = TRUE)] <- 0
+  siteDraw[which(is.na(siteDraw), arr.ind = TRUE)] <- 1
+  siteDraw <- matrix(as.numeric(siteDraw), nrow = nrow(siteMean), ncol = ncol(siteMean))
+  
+  Colour <- colorRampPalette(c("blue", "white", "red"))(200)
+  corrplot(siteMean, method = "color", col = Colour, type = "lower",
+           diag = FALSE, p.mat = siteDraw, tl.srt = 45)
+  
+}
+
