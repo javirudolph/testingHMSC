81931814ca871996dab23f47a675170c9642e72f
javirudolph <javiera.av@ufl.edu>
2019-04-23 11:13:17 -0400

    more written scripts

--- hmsc-evaluation/Niche_breadth_exploration/get_results_niche_breadth_exploration.R
+++
@@ -1,7 +0,0 @@
-# GET RESULTS - GENERATE FIGURES AND CHECK CONVERGENCE
-
-# Folderpaths
-fp_quad_wout <- "hmsc-evaluation/Niche_breadth_exploration/quad_wout"
-fp_quad_with <- "hmsc-evaluation/Niche_breadth_exploration/quad_with"
-fp_gaus_wout <- "hmsc-evaluation/Niche_breadth_exploration/gaus_wout"
-fp_gaus_with <- "hmsc-evaluation/Niche_breadth_exploration/gaus_with"
