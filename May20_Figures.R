789f3a60fdc8591c8603fe8b2e3358ae57107077
Javiera Rudolph <javirudolph@outlook.com>
2020-05-20 13:54:03 -0400

    Joining all species data

--- May20_Figures.R
+++ May20_Figures.R
@@ -25,44 +25,6 @@ if(dir.exists(tiff_path) == FALSE){
 scenarios <- c("FIG2A", "FIG2B", "FIG2C", "FIG2D", "FIG3A", "FIG3B", "FIG3C")
 new_scen <- LETTERS[1:7]
 
-# Theme -------------------------------------------------------
-
-mytheme <- function(data, plotMain = NULL, type = NULL){
-  data %>% 
-    ggtern(aes(x = env, z = spa, y = codist, size = r2, shape = iteration)) +
-    scale_T_continuous(limits=c(0,1),
-                       breaks=seq(0, 0.8,by=0.2),
-                       labels=seq(0,0.8, by= 0.2)) +
-    scale_L_continuous(limits=c(0,1),
-                       breaks=seq(0, 0.8,by=0.2),
-                       labels=seq(0, 0.8,by=0.2)) +
-    scale_R_continuous(limits=c(0,1),
-                       breaks=seq(0, 0.8,by=0.2),
-                       labels=seq(0, 0.8,by=0.2)) +
-    labs(title = plotMain,
-         x = "E",
-         xarrow = "Environment",
-         y = "Co",
-         yarrow = "Co-Distribution",
-         z = "S", 
-         zarrow = "Spatial Autocorrelation") +
-    theme_bw() +
-    theme_showarrows() +
-    theme_arrowlong() +
-    scale_shape_manual(values = c(21:25), guide = FALSE) +
-    theme(
-      #panel.grid = element_line(color = "darkgrey", size = 0.6),
-      plot.tag = element_text(size = 11),
-      plot.title = element_text(size = 11, hjust = 0.1 , margin = margin(t = 10, b = -20)),
-      tern.axis.arrow = element_line(size = 1),
-      tern.axis.arrow.text = element_text(size = 5),
-      axis.text = element_text(size = 4),
-      axis.title = element_text(size = 6),
-      legend.text = element_text(size = 6),
-      legend.title = element_text(size = 8)
-    ) +
-    guides(size = guide_legend(title = expression(R^2), order = 1))
-}
 # Figures data ----------------------------------------------------------------
 
 spp_data <- NULL
@@ -102,4 +64,42 @@ for (i in 1:7){
   
   
   sites_data <- bind_rows(sites_data, sites)
+}
+# Theme -------------------------------------------------------
+
+mytheme <- function(data, plotMain = NULL, type = NULL){
+  data %>% 
+    ggtern(aes(x = env, z = spa, y = codist, size = r2, shape = iteration)) +
+    scale_T_continuous(limits=c(0,1),
+                       breaks=seq(0, 0.8,by=0.2),
+                       labels=seq(0,0.8, by= 0.2)) +
+    scale_L_continuous(limits=c(0,1),
+                       breaks=seq(0, 0.8,by=0.2),
+                       labels=seq(0, 0.8,by=0.2)) +
+    scale_R_continuous(limits=c(0,1),
+                       breaks=seq(0, 0.8,by=0.2),
+                       labels=seq(0, 0.8,by=0.2)) +
+    labs(title = plotMain,
+         x = "E",
+         xarrow = "Environment",
+         y = "Co",
+         yarrow = "Co-Distribution",
+         z = "S", 
+         zarrow = "Spatial Autocorrelation") +
+    theme_bw() +
+    theme_showarrows() +
+    theme_arrowlong() +
+    scale_shape_manual(values = c(21:25), guide = FALSE) +
+    theme(
+      #panel.grid = element_line(color = "darkgrey", size = 0.6),
+      plot.tag = element_text(size = 11),
+      plot.title = element_text(size = 11, hjust = 0.1 , margin = margin(t = 10, b = -20)),
+      tern.axis.arrow = element_line(size = 1),
+      tern.axis.arrow.text = element_text(size = 5),
+      axis.text = element_text(size = 4),
+      axis.title = element_text(size = 6),
+      legend.text = element_text(size = 6),
+      legend.title = element_text(size = 8)
+    ) +
+    guides(size = guide_legend(title = expression(R^2), order = 1))
 }
