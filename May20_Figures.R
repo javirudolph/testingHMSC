4a4598eba2d8e3ab766802b96603e5725f333c33
Javiera Rudolph <javirudolph@outlook.com>
2020-05-22 13:08:19 -0400

    Added interaction matrices figure 5

--- May20_Figures.R
+++ May20_Figures.R
@@ -191,3 +191,92 @@ PQ %>%
   guides(size = guide_legend(title = expression(R^2), order = 1, nrow = 1, label.position = "bottom"),
          color = guide_colorbar(title = "Environmental\ndeviation", order = 2, barheight = 0.3))
 ggsave(paste0(tiff_path, "Figure3.tiff"), dpi = 600, width = 6, height = 6)
+
+# Figure 4 ----------------------------------------
+
+# Interaction matrices functions ----------------------------------------
+
+get_upper_tri <- function(cormat){
+  cormat[lower.tri(cormat, diag = TRUE)]<- NA
+  return(cormat)
+}
+
+intplot <- function(modelfile, iteration){
+  assoMat <- corRandomEff(modelfile[[iteration]])
+  siteMean <- apply(assoMat[ , , , 1], 1:2, mean)
+  
+  siteMean %>% 
+    get_upper_tri() %>% 
+    as_tibble() %>% 
+    rownames_to_column(., var = "Specie1") %>% 
+    pivot_longer(-Specie1, names_to = "Specie2", values_to = "value") %>% 
+    drop_na(value) %>% 
+    mutate(iteration = paste("Iteration", iteration),
+           Specie2 = str_replace(Specie2, "y", ""),
+           Specie1 = factor(Specie1, levels = c(1:12)),
+           Specie2 = factor(Specie2, levels = c(12:1)),
+           color = ifelse(value > 0.4, "red",
+                          ifelse(value < -0.4, "blue", NA)), 
+           signi = ifelse(is.na(color)== TRUE, "x", NA))
+}
+
+# The interaction matrix we provided:
+A <- matrix(0,nr=12,nc=12)
+d <- as.matrix(dist(c(1:12),upper=TRUE,diag=T))
+A[d<=1] = -1
+diag(A) = 0
+
+A %>% 
+  get_upper_tri( ) %>% 
+  as_tibble() %>% 
+  rownames_to_column(., var = "Specie1") %>% 
+  pivot_longer(-Specie1, names_to = "Specie2", values_to = "value") %>% 
+  drop_na(value) %>% 
+  mutate(iteration = "Simulation",
+         Specie2 = str_replace(Specie2, "V", ""),
+         Specie1 = factor(Specie1, levels = c(1:12)),
+         Specie2 = factor(Specie2, levels = c(12:1)),
+         color = ifelse(value > 0.4, "red",
+                        ifelse(value < -0.4, "blue", NA)), 
+         signi = ifelse(is.na(color)== TRUE, "x", NA)) -> orig_matrix
+
+# Figure 5 ----------------------------------------------
+modelfile <- readRDS(paste0(outsfolderpath, "FIG3C", "-model.RDS"))
+
+intData <- NULL
+for(i in 1:5){
+  a <- intplot(modelfile, i)
+  intData <- bind_rows(intData, a)
+}
+
+intData %>% 
+  bind_rows(intData, orig_matrix) %>% 
+  mutate(iteration = str_replace(iteration,"Iteration", "Replicate"),
+         iteration = factor(iteration, levels = c("Simulation", "Replicate 1", "Replicate 2", "Replicate 3",
+                                                  "Replicate 4", "Replicate 5"))) %>% 
+  ggplot(., aes(x = Specie1, y = Specie2, fill = value)) +
+  facet_wrap(~iteration, ncol = 6) +
+  geom_tile() +
+  geom_text(aes(label = signi), size = 2)+
+  scale_fill_gradient2(
+    high = "#4d4d4d", mid = "white", low = "#cb181d",
+    limits = c(-1, 1)) +
+  #theme_bw() + 
+  ggplot2::theme_minimal()+
+  theme(legend.position = "right",
+        axis.text = element_text(size = 4),
+        axis.text.x = element_text(angle = 90),
+        #axis.text = element_blank(),
+        #axis.title = element_text(size = 6),
+        axis.title = element_blank(),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 8), 
+        strip.text = element_text(size = 6, margin = margin(0.1,0.1,0.1,0.1, "cm")),
+        strip.background = element_rect(color = "black", fill = "#c4c4bb")) +
+  guides(fill = guide_colorbar(title = NULL,barwidth = 0.3, barheight = 5))
+
+ggsave(paste0(tiff_path, "Figure5.tiff"), dpi = 600, width = 6, height = 1.5)
+
+
+# SUPPLEMENT------------------------------------------------
+# Interaction matrices for all scenarios
