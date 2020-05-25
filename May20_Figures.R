c1879d52ceb230d3b1aa0511330507481b26014c
Javi Rudolph <javiera.av@ufl.edu>
2020-05-25 19:25:51 -0400

    figure 4 using encircle for low dispersal

--- May20_Figures.R
+++ May20_Figures.R
@@ -161,7 +161,7 @@ PQ %>%
   geom_point(aes(color = Edev, fill = Edev), alpha = 0.7) +
   scale_size_area(limits = c(0, 1), breaks = seq(0, 1, 0.2))  +
   scale_fill_viridis_c(guide = "none", na.value = "#000000") +
-  scale_color_viridis_c(na.value = "#000000") +
+  scale_color_viridis_c(na.value = "#000000", limits = c(0,0.5)) +
   facet_grid(type1~type2, switch = "y") +
   theme(
     legend.position = "bottom",
@@ -180,7 +180,7 @@ PQ %>%
   geom_point(aes(color = Edev, fill = Edev), alpha = 0.7) +
   scale_size_area(limits = c(0, 1), breaks = seq(0, 1, 0.2))  +
   scale_fill_viridis_c(guide = "none", na.value = "#000000") +
-  scale_color_viridis_c(na.value = "#000000") +
+  scale_color_viridis_c(na.value = "#000000", limits = c(0, 0.5)) +
   facet_grid(type1~type2, switch = "y") +
   theme(
     tern.axis.arrow.text = element_text(size = 7),
@@ -218,28 +218,30 @@ bind_rows(W, R) %>%
   mutate(type = factor(type, levels = c("Species", "Sites"))) -> WR
 head(WR)
 
-WR %>%  
+WR %>%
   mytheme() +
   facet_wrap(~type, ncol=2) +
-  geom_encircle(data = WR %>% 
-                  filter(dispersal == 0.01), aes(group = dispersal), size = 0.2, fill = "yellow", alpha = 0.1, expand = 0.01) +
-  geom_point(aes(color = Edev, fill = nicheOptima), alpha = 0.9) +
+  geom_encircle(data = WR %>% filter(dispersal == 0.01),
+                aes(group = dispersal), size = 0.7, color = NA, fill = "#FDE4A6FF",alpha = 0.6, expand = 0.02) +
+  geom_point(aes(color = Edev, fill = nicheOptima), alpha = 0.8) +
   scale_size_area(limits = c(0, 1), breaks = seq(0, 1, 0.2))  +
-  #scale_fill_viridis_c(option = "magma", na.value = "#000000") +
-  scale_fill_gradient2(high = "#71198E", mid = "#8C959F", low = "#BD2466", limits = c(0, 1), midpoint = 0.5) +
-  scale_color_viridis_c(na.value = "#000000", limits = c(0, 0.5)) +
+  #scale_fill_viridis_c(na.value = "#666666") +
+  scale_fill_gradient2(high = "#000004FF", low = "#400F73FF", mid = "#F4685CFF", limits = c(0, 1), midpoint = 0.5) +
+  scale_color_viridis_c(na.value = "white", limits = c(0, 0.5)) +
   theme(
     legend.position = "bottom",
     #legend.box = "vertical",
-    legend.spacing.y = unit(0.01, "in")
+    #legend.spacing.y = unit(0.01, "in"),
+    legend.text = element_text(size = 4),
+    legend.title = element_text(size = 6)
   ) +
   guides(size = guide_legend(title = expression(R^2), order = 2, nrow = 1, label.position = "bottom"),
-         fill = guide_colorbar(title = "Niche \noptima", order = 1, barheight = 0.3),
-         color = guide_colorbar(title = "Environmental\ndeviation", order = 3, barheight = 0.3))
+         fill = guide_colorbar(title = "Niche \noptima", order = 1, barheight = 0.5),
+         color = guide_colorbar(title = "Environmental\ndeviation", order = 3, barheight = 0.5))
 
-ggsave(paste0(tiff_path, "Figure4.tiff"), dpi = 600, width = 6, height = 4)
+ggsave(paste0(tiff_path, "Figure4.tiff"), dpi = 600, width = 6, height = 4.5)
 
-
+scales::show_col(viridis::viridis_pal(option = "A")(20))
 
 # Interaction matrices functions and data----------------------------------------
 
